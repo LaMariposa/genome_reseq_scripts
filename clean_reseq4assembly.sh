a99bcb46043ae4457ae4e8d5c7eb1df641a7551a
MSupple <masupple@ncsu.edu>
2013-08-07 23:27:31 -0400

    initial

---
+++ clean_reseq4assembly.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+#clean_reseq4assembly.sh
+#script to clean whole genome resequencing reads prior to assembly
+#Megan Supple
+#9 June 2013
+
+#usage: clean_reseq4assembly.sh <clean.reseq.input>
+#<clean.reseq.input> is a text file with entries as in example input file (see example.clean.reseq.input or example at the end of this script)
+
+#requires: Trimmomatic, FLASh, JELLYFISH, Quake, FastQC.
+
+#produces output in the current directory: 
+	#clean.log			-- output from stdout and stderr
+	#*stats.txt 			-- stats from error correction
+	#<sample>.hist* 		-- kmer distribution
+	#*.fastqc*			-- FastQC reports
+	#<sample>.pair1.fastq.gz	--**cleaned reads, pair 1**
+	#<sample>.pair1.fastq.gz	--**cleaned reads, pair 2**
+	#GS012.single.fastq.gz		--**cleaned reads, unpaired**
+
+
+
+#read in input file
+source $1
+#set output file for stdout
+exec &>clean.log
+
+
+echo "cleaning  data" > $resourcelog
+echo "The number of Ns remaining after cleaning" > $Nlog
+
+#loop over each sample
+for ((a=0; a<${#id[@]}; a++))
+	do
+		echo -e "\ncleaning sample ${id[a]}"	
+		echo -e "\ncleaning sample ${id[a]}" >> $resourcelog
+		
+		#adapter trim, merge completely overlapping reads, and quality filtering with Trimmomatic
+		echo -e "\ntrimmomatic"
+		echo -e "\ntrimmomatic" >> resource.log
+		if [ $score = "-phred33" ]
+		  then
+			/usr/bin/time -v -o $resourcelog -a java -classpath $path_trimmomatic org.usadellab.trimmomatic.TrimmomaticPE -threads $threads $score $files/${file1[a]} $files/${file2[a]} ${id[a]}.trim.p1.fastq ${id[a]}.trim.u1.fastq ${id[a]}.trim.p2.fastq ${id[a]}.trim.u2.fastq ILLUMINACLIP:$adapter:$seedMismatches:$palindromeClipThreshold:$simpleClipThreshold HEADCROP:$headCropLen SLIDINGWINDOW:$windowSize:$windowQuality LEADING:$leadQuality TRAILING:$trailQuality 
+		elif [ $score = "-phred64" ]
+		  then
+                        /usr/bin/time -v -o $resourcelog -a java -classpath $path_trimmomatic org.usadellab.trimmomatic.TrimmomaticPE -threads $threads $score $files/${file1[a]} $files/${file2[a]} ${id[a]}.trim.p1.fastq ${id[a]}.trim.u1.fastq ${id[a]}.trim.p2.fastq ${id[a]}.trim.u2.fastq ILLUMINACLIP:$adapter:$seedMismatches:$palindromeClipThreshold:$simpleClipThreshold HEADCROP:$headCropLen SLIDINGWINDOW:$windowSize:$windowQuality LEADING:$leadQuality TRAILING:$trailQuality TOPHRED33
+		else
+		  echo "$score is not a valid quality score for trimmomatic. Should be -phred33 or -phred64"; exit 1
+		fi  
+
+		#merge overlapping reads with FLASH
+		echo -e "\nflash"
+		echo -e "\nflash" >> $resourcelog
+		/usr/bin/time -v -o $resourcelog -a flash -o ${id[a]} -t $threads -m $minOverlap -x $maxMismatchDensity -p 33 -M $maxOverlap ${id[a]}.trim.p1.fastq ${id[a]}.trim.p2.fastq
+
+		#error correct with jellyfish and quake
+                #use jellyfish to count kmers
+		echo -e "\njellyfish"
+		echo -e "\njellyfish" >> $resourcelog
+                /usr/bin/time -v -o $resourcelog -a jellyfish count -m $quakeK -q -C -t $threads -s 2000000000 --quality-start 33 --stats=Stats -o ${id[a]}.jelly ${id[a]}.notCombined_1.fastq ${id[a]}.notCombined_2.fastq ${id[a]}.trim.u1.fastq ${id[a]}.trim.u2.fastq ${id[a]}.extendedFrags.fastq
+
+		#check if need to merge
+		if ( test -e ${id[a]}.jelly_1 )
+			then /usr/bin/time -v -o $resourcelog -a jellyfish qmerge -m $quakeK -s 4000000000 -o ${id[a]}.jf.output ${id[a]}.jelly_*
+			else mv ${id[a]}.jelly_0 ${id[a]}.jf.output
+		fi
+                
+		#create counts file
+		/usr/bin/time -v -o $resourcelog -a jellyfish qdump -c -t -o ${id[a]}.counts ${id[a]}.jf.output
+
+                #make a file list to input into correct
+                touch file.list
+                echo "${id[a]}.notCombined_1.fastq ${id[a]}.notCombined_2.fastq" >> file.list
+                echo "${id[a]}.trim.u1.fastq" >> file.list
+		echo "${id[a]}.trim.u2.fastq" >> file.list
+		echo "${id[a]}.extendedFrags.fastq" >> file.list
+
+                #use quake to correct the reads
+		echo -e "\nquake"
+		echo -e "\nquake" >> $resourcelog
+                /usr/bin/time -v -o $resourcelog -a $path_quake/bin/correct -f file.list -k $quakeK -c $cov_cut -l $minlen -m ${id[a]}.counts -p $threads -q 33
+
+		#merge single end fastqs
+		echo -e "\ncat"
+		echo -e "\ncat" >> $resourcelog
+		/usr/bin/time -v -o $resourcelog -a cat ${id[a]}.extendedFrags.cor.fastq ${id[a]}.trim.u2.cor.fastq ${id[a]}.trim.u1.cor.fastq ${id[a]}.notCombined_1.cor_single.fastq ${id[a]}.notCombined_2.cor_single.fastq > ${id[a]}.single.fastq
+		
+		#rename final paired files
+		mv ${id[a]}.notCombined_1.cor.fastq ${id[a]}.pair1.fastq
+		mv ${id[a]}.notCombined_2.cor.fastq ${id[a]}.pair2.fastq 	
+
+
+		#FastQC final files
+		echo -e "\nFastQC"
+		fastqc -t $threads ${id[a]}.pair1.fastq
+		fastqc -t $threads ${id[a]}.pair2.fastq
+		fastqc -t $threads ${id[a]}.single.fastq
+		
+		
+		echo "The number of reads containing Ns in file ${id[a]}.pair1.fastq is" >> $Nlog
+		grep -B1 'N' ${id[a]}.pair1.fastq | grep '^@' | wc -l >> $Nlog
+		echo "and this many reads have more than 1 N:" >> $Nlog
+		grep -B1 'N' ${id[a]}.pair1.fastq | grep -A1 '^@' | awk -FN '{if (NF>2) print NF-1}' | wc -l >> $Nlog
+		
+		echo "The number of reads containing Ns in file ${id[a]}.pair2.fastq is" >> $Nlog
+		grep -B1 'N' ${id[a]}.pair2.fastq | grep '^@' | wc -l >> $Nlog
+		echo "and this many reads have more than 1 N:" >> $Nlog
+		grep -B1 'N' ${id[a]}.pair2.fastq | grep -A1 '^@' | awk -FN '{if (NF>2) print NF-1}' | wc -l >> $Nlog
+		
+		echo "The number of reads containing Ns in file ${id[a]}.single.fastq is" >> $Nlog
+		grep -B1 'N' ${id[a]}.single.fastq | grep '^@' | wc -l >> $Nlog
+		echo "and this many reads have more than 1 N:" >> $Nlog
+		grep -B1 'N' ${id[a]}.single.fastq | grep -A1 '^@' | awk -FN '{if (NF>2) print NF-1}' | wc -l >> $Nlog
+
+
+		#gzip final files and remove intermediates
+		gzip ${id[a]}.pair1.fastq &
+		gzip ${id[a]}.pair2.fastq &
+		gzip ${id[a]}.single.fastq &
+
+		rm ${id[a]}.trim.u1.fastq ${id[a]}.trim.u2.fastq ${id[a]}.trim.p1.fastq ${id[a]}.trim.p2.fastq ${id[a]}.notCombined_1.fastq ${id[a]}.notCombined_2.fastq ${id[a]}.extendedFrags.fastq ${id[a]}.notCombined_1.cor_single.fastq ${id[a]}.notCombined_2.cor_single.fastq ${id[a]}.trim.u1.cor.fastq ${id[a]}.trim.u2.cor.fastq ${id[a]}.extendedFrags.cor.fastq ${id[a]}.counts error_model.${id[a]}.* ${id[a]}.jf.output file.list
+
+		echo -e "\ndone cleaning sample ${id[a]}\n\n\n" 
+ 		
+	done
+
+echo DONE!!!
+
+echo | mutt -s "reseq_assembly_clean.sh script from $1 complete" $email
